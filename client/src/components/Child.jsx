5bf3165d8b28ebecba07a01da6b563ca3d1f8328
Alex McGinnis <alex.z.mcginnis@gmail.com>
2021-10-16 16:01:49 -0500

    base

---
+++ client/src/components/Child.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Child = () =>{
+  return (
+    <div>
+      this is a child
+    </div>
+  )
+}
+
+export default Child
--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import Chat from './Chat.jsx'
+import Child from './Child.jsx';
 //any time extend from class need to create a constructor then invoke super in the constructor
 const App = () => {
 
     return (
       <div>
-        <h1>Live Chat</h1>
-        <Chat />
+        <h1>app</h1>
+        <Child />
       </div>
     )
   }
